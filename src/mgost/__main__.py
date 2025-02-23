8084c2c7f8c808600ad2f92748e7c49a317c3fe5
Articha <tima-1324@mail.ru>
2025-02-23 23:31:16 +0300

    Added cli interface without `python -m`

--- src/mgost/__main__.py
+++ src/mgost/__main__.py
@@ -1,14 +1,4 @@
-from pathlib import Path
-from .settings import init_settings
+from . import cli
 
-from . import _parse_args, convert
-
-
-def main():
-    with init_settings(Path() / 'mgost'):
-        source, dest = _parse_args()
-        convert(source, dest)
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    cli.main()
