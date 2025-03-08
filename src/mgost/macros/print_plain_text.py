23765fc20272b9e7467caa8fa09f4cbe4d79bf5f
Articha <tima-1324@mail.ru>
2025-03-08 22:45:47 +0300

    • Fixed old bug with empty first page • Added check for macros `print_plain_text` if target file exists

--- src/mgost/macros/print_plain_text.py
+++ src/mgost/macros/print_plain_text.py
@@ -1,6 +1,7 @@
 from io import StringIO
 from multiprocessing import Process, Queue
 from pathlib import Path
+from logging import warning
 
 from ._mixins import AfterDocxCreation, DuringDocxCreation
 from mgost.settings import Settings
@@ -10,22 +11,35 @@ def exec_code(file_path: Path, q: Queue):
     code = file_path.read_text(encoding='utf-8')
     import sys
     sys.stdout = StringIO()
-    exec(code)
-    q.put(sys.stdout.getvalue())
+    try:
+        exec(code)
+    except BaseException as e:
+        text = (
+            f"<Exception in {file_path.name}: "
+            f"{type(e).__qualname__}({e}).>"
+        )
+        warning(text[1:-1])
+    else:
+        text = sys.stdout.getvalue()
+
+    q.put(text)
 
 
 class Macros(DuringDocxCreation, AfterDocxCreation):
     """Prints simple python code from stdout into document"""
     __slots__ = ('process', 'q', 'run')
-    process: Process
+    process: Process | None
     q: Queue
 
     def process_during_docx_creation(self, p, context):
+        self.process = None
         file_name = self.macros.value
         current_folder = context.current_file_path.parent
         file_path = current_folder / file_name
         if not file_path.exists():
-            raise RuntimeError(f"File {file_name} does not exist")
+            text = f"<File {file_name} does not exist>"
+            warning(text[1:-1])
+            return [p.add_run(text)]
 
         self.q = Queue(maxsize=1)
         self.process = Process(
@@ -42,15 +56,18 @@ class Macros(DuringDocxCreation, AfterDocxCreation):
     def process_after_docx_creation(
         self, context
     ) -> None:
+        assert self.macros.runs is not None
+        assert len(self.macros.runs) == 1
+        if self.process is None:
+            return
         try:
             self.process.join(timeout=Settings.get().code_run_timeout)
         except TimeoutError:
-            print(f"Timeout for code in {self.macros.value}")
-            return
+            text = f"<Timeout for code in {self.macros.value}>"
+            warning(text[1:-1])
+            self.macros.runs[0].text = text
         assert self.q.full(), self.q.qsize()
         received = self.q.get()
         assert isinstance(received, str)
         received = received.strip()
-        assert self.macros.runs is not None
-        assert len(self.macros.runs) == 1
         self.macros.runs[0].text = received
